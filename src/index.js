cb8093e34797dc8b07e8c2a1930eaa0046264137
hossam-khalaf <hossamhassan303089@gmail.com>
2020-11-22 21:57:32 +0200

    First, geolocation with error msg

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //intialiaze the state = only direct assignment
+    this.state = { lat: null, errorMsg: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // call setState
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => this.setState({ errorMsg: err.message })
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          Latitude is:{' '}
+          {this.state.errorMsg
+            ? this.state.errorMsg
+            : Math.floor(this.state.lat)}
+          <SeasonDisplay />
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
